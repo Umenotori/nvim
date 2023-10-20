d214a948c6de4982690806baaaf6d62589397393
Tadashi <mtadashi@hotmail.com>
2023-10-20 17:09:31 -0300

    Modified packer

--- lua/Tadashi/packer.lua
+++ lua/Tadashi/packer.lua
@@ -11,7 +11,7 @@ return require('packer').startup(function(use)
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use 'rebelot/kanagawa.nvim'  
+  use 'rebelot/kanagawa.nvim'
   use {
       'nvim-treesitter/nvim-treesitter',
       run = function()
