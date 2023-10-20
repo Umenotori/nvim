f8b178d2df09a316dab5333094f2dff24f8e7a23
Tadashi <mtadashi@hotmail.com>
2023-10-20 06:55:48 -0300

    finalized setup of Neovim

--- README.md
+++ README.md
@@ -12,6 +12,32 @@ The bare minimum plugins are:
 ## Complementary configurations
 
 - Custom Keymaps
-- Custom Theme
+- Custom Theme (Kanagawa dragon)
+- file swapping (harpoon.nvim)
 - undotree (undotree.nvim)
 - git integration (fugitive.nvim)
+- refactoring (refactoring.nvim)
+- treesitter context
+- diagnostics (trouble.nvim)
+
+## General Instructions
+
+It's needed to :source (:so) the packer.lua file everytime a plugin is changed, including on install.
+Almost every plugin includes a file in after/plugin for settings.
+
+## Custom Keymaps Used
+
+### Nvim
+
+vim.g.mapleader = " " (<leader> key = space)
+<leader>pv -> return to netrw
+
+
+### Nvim Telescope
+
+<leader>pf -> find files
+<C-p>      -> git files
+<leader>ps -> grep
+<leader>vh -> help tags
+
+
--- lua/Tadashi/packer.lua
+++ lua/Tadashi/packer.lua
@@ -11,6 +11,45 @@ return require('packer').startup(function(use)
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
-  
+  use 'rebelot/kanagawa.nvim'  
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }
+  use 'ThePrimeagen/harpoon'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      requires = {
+          --- Uncomment these if you want to manage LSP servers from neovim
+           {'williamboman/mason.nvim'},
+           {'williamboman/mason-lspconfig.nvim'},
+
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'L3MON4D3/LuaSnip'},
+      }
+  }
+  use({
+      "folke/trouble.nvim",
+      config = function()
+          require("trouble").setup {
+              icons = false,
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+          }
+      end
+  })
+  use("nvim-treesitter/nvim-treesitter-context")
+  use("theprimeagen/refactoring.nvim")
 
 end)
